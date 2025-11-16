8c5398c3baf1ba9032ed4f64eb147cf0123dd675
Benoît LAHOZ <lahoz.ben@gmail.com>
2025-11-16 18:39:05 +0100

    Nuxt Config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/content", "@nuxt/eslint", "@nuxt/ui"]
+  modules: ["@nuxt/content", "@nuxt/eslint", "@nuxt/ui"],
+  robots: {
+    robotsTxt: false,
+  },
+  llms: {
+    domain: 'https://benoitlahoz.github.io/vue-checkin/',
+    title: 'Vue Check-in',
+    description: 'Documentation for Vue Check-in composable and features',
+  }
 })
