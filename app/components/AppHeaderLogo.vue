41622375e6600071f57aad0432223706ea7b074a
Benoît LAHOZ <lahoz.ben@gmail.com>
2025-11-17 12:10:20 +0100

    Docs

---
+++ app/components/AppHeaderLogo.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex items-center">
+    <img src="/vue-checkin.png" alt="VueCheckIn Logo" class="h-6 w-6 mr-4" />
+    <span class="font-semibold text-lg">VueCheckIn</span>
+  </div>
+</template>
